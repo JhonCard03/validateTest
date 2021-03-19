30f0af150b84f34f6fdf0e13fc05fe15418a638b
Jhonatan Cardenas <cardenasgon@hotmail.com>
2021-03-19 17:08:49 -0500

    acceso app

---
+++ src/main/java/main/main.java
@@ -0,0 +1,11 @@
+package main;
+
+import pom.HomePage;
+
+public class main {
+
+    public static void main(String[] args) {
+        HomePage h = new HomePage();
+        h.loginK();
+    }
+}
--- src/main/java/pom/HomePage.java
+++ src/main/java/pom/HomePage.java
@@ -1,6 +1,46 @@
 package pom;
 
-public class HomePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.BeforeTest;
 
+public class HomePage {
+    String page = "https://amrs-dev.engkantar.com";
+    WebDriver driver;
+    String pathChrome =  System.getProperty("user.dir") + "\\drivers\\chromedriver.exe";
+
+
+
+    @BeforeTest
+    public void loginK(){
+
+        String user =  "testqa";
+        String password = "#testqa@";
+        System.setProperty("webdriver.chrome.driver",pathChrome);
+        driver = new ChromeDriver();
+        driver.get(page);
+        driver.manage().window().maximize();
+
+
+        WebDriverWait wait = new WebDriverWait(driver,60);
+
+        WebElement usr = wait.until(ExpectedConditions.elementToBeClickable(By.name("user")));
+        usr.clear();
+        usr.click();
+        usr.sendKeys(user);
+
+        WebElement pwd = wait.until(ExpectedConditions.elementToBeClickable(By.name("password")));
+        pwd.clear();
+        pwd.click();
+        pwd.sendKeys(password);
+
+        WebElement conectar = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"login-form\"]/footer/button")));
+        conectar.click();
+
+    }
 
 }
